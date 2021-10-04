6fb10964d95e6ca4d816f08fa41aca59c7969887
Korotkov Dmitry <2110821@mail.ru>
2021-10-04 07:26:08 +0300

    Заготовка теста

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -1,15 +1,37 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class CardOrderTest {
+    private WebDriver driver;
 
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chromr.driver", "..//driver//win//chromedriver.exe");
+    }
 
-    @Test
-    public void shouldOrderCard () {
+    @BeforeEach
+    void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+
+    }
+
+    @Test
+    public void shouldOrderCard() {
     }
 }
